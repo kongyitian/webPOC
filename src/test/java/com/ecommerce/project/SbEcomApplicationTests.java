e35d9a2f2d0543372195149ac08ee8fed71fbd12
Yitian Kong <yitian.kong@ing.com>
2025-05-19 11:41:14 +0200

    start ecommerce

--- src/test/java/com/ecommerce/project/SbEcomApplicationTests.java
+++ src/test/java/com/ecommerce/project/SbEcomApplicationTests.java
@@ -1,10 +1,10 @@
-package com.embarkx.FirstSpring;
+package com.ecommerce.project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FirstSpringApplicationTests {
+class SbEcomApplicationTests {
 
 	@Test
 	void contextLoads() {
